4a5b217a2005bde06c4030ae02364c51aed354be
Chen-Rong-Zi <1398881912@qq.com>
2026-08-05 14:45:36 +0800

    feat: add date continuity check to getCampusConsumptionTrend

--- docs/js/data-service.js
+++ docs/js/data-service.js
@@ -1838,8 +1838,11 @@ const DataService = {
 
         for (let i = 1; i < dates.length; i++) {
           const date = dates[i];
-          const prev = bh[dates[i - 1]];
+          const prevDate = dates[i - 1];
+          const prev = bh[prevDate];
           const curr = bh[date];
+          // 日期不连续时跳过，不跨日累计
+          if (!this._isConsecutiveDates(date, prevDate)) continue;
           // 消耗 = max(0, 前日余额 - 当日余额)，充值或不变记为0
           const cons = prev > curr ? prev - curr : 0;
           allDateSet.add(date);
